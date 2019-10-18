724acccd053c3ea7c9d1fd9a6b5c2aff8f438564
Aditya Azad <adityaazad121@gmail.com>
2019-10-18 14:11:58 -0400

    Separated the custom set variables from the init file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,6 +5,10 @@
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
+;; custom-set-variables and custom-set-faces file in custom.el file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
 ;; bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -23,26 +27,3 @@
   :ensure t
   :config
   (load-theme 'gruvbox))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(custom-safe-themes
-   (quote
-    ("1436d643b98844555d56c59c74004eb158dc85fc55d2e7205f8d9b8c860e177f" default)))
- '(menu-bar-mode nil)
- '(package-selected-packages
-   (quote
-    (gruvbox-theme use-package gruber-darker-theme evil)))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "outline" :family "Consolas")))))
