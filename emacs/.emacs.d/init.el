2947b2263f26034c501a65b54913632ed47a97f3
Aditya Azad <azada@sslab21.cs.purdue.edu>
2019-10-18 15:02:57 -0400

    Auto maximize and undo limit changed

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,29 +1,42 @@
-;; load package manager
+; load package manager
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
-;; custom-set-variables and custom-set-faces file in custom.el file
+; custom-set-variables and custom-set-faces file in custom.el file
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file 'noerror)
 
-;; bootstrap use-package
+; bootstrap use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile
   (require 'use-package))
 
-;; load evil
+; PACKAGES
+; ========
+
+; load evil
 (use-package evil
     :ensure t
     :init
     (evil-mode))
 
-;; load gruvbox theme
+; load gruvbox theme
 (use-package gruvbox-theme
   :ensure t
   :config
   (load-theme 'gruvbox))
+
+; OTHER SETTINGS
+; ==============
+
+; maximize 
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+; setting very high limits for undo buffers
+(setq undo-limit 20000000)
+(setq undo-strong-limit 40000000)
