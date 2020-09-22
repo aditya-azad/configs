06219a61acfc8aace42804fe5c993b532a46442d
Aditya Azad <adityaazad121@gmail.com>
2020-09-22 02:48:43 -0400

    Added config copier file for windows

---
+++ nvim-config-copier.bat
@@ -0,0 +1,4 @@
+copy C:\Users\aditya\AppData\Local\nvim\init.vim .\configs\.config\nvim\
+copy C:\Users\aditya\AppData\Local\nvim\snips .\configs\configs\.config\nvim\snips
+git commit -a -m "Updated nvim config"
+git push
