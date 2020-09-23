5e9c4200de36c3026b2ec9c1e9929af3b0ee8a1d
Aditya Azad <adityaazad121@gmail.com>
2020-09-23 02:01:43 -0400

    Updated nvim config

--- nvim-config-copier.bat
+++ nvim-config-copier.bat
@@ -1,4 +1,4 @@
 robocopy C:\Users\aditya\AppData\Local\nvim\init.vim .\configs\.config\nvim\
-robocopy C:\Users\aditya\AppData\Local\nvim\snips\* .\configs\configs\.config\nvim\snips\* /E
+robocopy C:\Users\aditya\AppData\Local\nvim\snips .\configs\configs\.config\nvim\snips /E
 git commit -a -m "Updated nvim config"
 git push
