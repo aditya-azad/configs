dac223bef9bef44736f36930b30324763c7543a6
Aditya Azad <adityaazad121@gmail.com>
2020-10-04 02:34:03 -0400

    Updated nvim config

--- nvim-config-copier.bat
+++ nvim-config-copier.bat
@@ -9,12 +9,10 @@ IF "%1" == "t" GOTO Copyto
 
 :Copyto
 copy %NVIM_CONFIG%\init.vim %NVIM_HOME%\
-robocopy %NVIM_CONFIG%\snips\ %NVIM_HOME%\snips\ /E /it /is
 GOTO End
 
 :Copyfrom
 copy %NVIM_HOME%\init.vim %NVIM_CONFIG%\
-robocopy %NVIM_HOME%\snips\ %NVIM_CONFIG%\snips\ /E /it /is
 git add .
 git commit -m "Updated nvim config"
 git push
