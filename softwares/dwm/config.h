225c268784a2ad079f83fefb7f9420f19fd8cfa6
Aditya Azad <adityaazad121@gmail.com>
2020-10-11 20:03:24 +0530

    Cleanup and added tilegaps to dwm

--- softwares/dwm/config.h
+++ softwares/dwm/config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 0;  /* border pixel of windows */
 static const unsigned int snap      = 32; /* snap pixel */
+static const unsigned int gappx     = 10; /* gap pixel between windows */
 static const int swallowfloating    = 0;  /* 1 means swallow floating windows by default */
 static const int rmaster            = 0;  /* 1 means master-area is initially on the right */
 static const int showbar            = 1;  /* 0 means no bar */
