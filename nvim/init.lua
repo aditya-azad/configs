d87c63a4291c18f126ae56ca09795894b1ff3d85
Aditya Azad <adityaazad121@gmail.com>
2023-12-19 10:33:06 -0500

    switch to lazy nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -11,11 +11,11 @@ end
 
 -- general
 
-vim.opt.encoding = 'utf-8'
+vim.opt.encoding = "utf-8"
 vim.opt.hidden = true
 vim.opt.updatetime = 100
-vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
-vim.opt.backspace:append { 'indent', 'eol', 'start' }
+vim.opt.clipboard:append { "unnamed", "unnamedplus" }
+vim.opt.backspace:append { "indent", "eol", "start" }
 
 -- spacing
 
@@ -29,15 +29,15 @@ vim.opt.expandtab = true
 -- formatting
 
 vim.opt.wrap = false
-vim.opt.formatoptions = 'qln'
+vim.opt.formatoptions = "qln"
 vim.opt.list = true
 vim.opt.listchars = {
-    tab = '→ ',
-    space = '.',
-    extends = '~',
+    tab = "→ ",
+    space = ".",
+    extends = "~",
 }
 vim.opt.fillchars = {
-    vert = '│',
+    vert = "│",
 }
 
 -- searching
@@ -45,22 +45,22 @@ vim.opt.fillchars = {
 vim.opt.incsearch = true
 vim.opt.showmatch = true
 vim.opt.smartcase = true
-vim.opt.shortmess = 'filnxtToOFc'
+vim.opt.shortmess = "filnxtToOFc"
 
 -- mouse
 
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- numbers and side column
 
-vim.opt.signcolumn = 'number'
+vim.opt.signcolumn = "number"
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- display all matching names when tab completes
 
 vim.opt.wildmenu = true
-vim.opt.wildmode = 'full'
+vim.opt.wildmode = "full"
 vim.opt.hlsearch = false
 vim.opt.laststatus = 2
 
@@ -80,14 +80,13 @@ vim.opt.swapfile = true
 vim.opt.undofile = true
 vim.opt.writebackup = true
 vim.opt.undolevels = 1000
-vim.opt.undodir = os.getenv('HOME') .. '/.vim-tmp/undo'
-vim.opt.backupdir = os.getenv('HOME') .. '/.vim-tmp/backup'
-vim.opt.directory = os.getenv('HOME') .. '/.vim-tmp/swap'
+vim.opt.undodir = os.getenv("HOME") .. "/.vim-tmp/undo"
+vim.opt.backupdir = os.getenv("HOME") .. "/.vim-tmp/backup"
+vim.opt.directory = os.getenv("HOME") .. "/.vim-tmp/swap"
 
 -- colors
 
 vim.opt.termguicolors = true
-vim.cmd.colorscheme("catppuccin")
 vim.o.background = "dark"
 
 -- scroll
@@ -96,92 +95,97 @@ vim.opt.scrolloff = 8
 
 -- general keymaps
 
-vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>ev', ':e $MYVIMRC<CR>')
-vim.keymap.set('n', '<leader>o', ':NvimTreeToggle<CR>')
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', 'H', ':help <C-r><C-w><CR>')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
-vim.keymap.set('x', '<leader>p', '\"_dp')
-vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
-vim.keymap.set('n', '<leader>tw4', '<cmd>lua SetTabWidth(4)<CR>')
-vim.keymap.set('n', '<leader>tw2', '<cmd>lua SetTabWidth(2)<CR>')
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>ev", ":e $MYVIMRC<CR>")
+vim.keymap.set("n", "<leader>o", ":NvimTreeToggle<CR>")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "H", ":help <C-r><C-w><CR>")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("x", "<leader>p", "\"_dp")
+vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+vim.keymap.set("n", "<leader>tw4", "<cmd>lua SetTabWidth(4)<CR>")
+vim.keymap.set("n", "<leader>tw2", "<cmd>lua SetTabWidth(2)<CR>")
 
 ------------------------------------------------------------------------ plugins
 
--- packer
+-- lazy nvim
 
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-require('packer').startup(function(use)
-    use('wbthomason/packer.nvim')
-    use('lewis6991/gitsigns.nvim')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-    use('nvim-tree/nvim-tree.lua')
-    use('hrsh7th/cmp-nvim-lua')
-    use('nvim-tree/nvim-web-devicons')
-    use({
-        'catppuccin/nvim',
-        as = 'catppuccin'
-    })
-    use('nvim-lualine/lualine.nvim')
-    use({
-        'nvim-telescope/telescope.nvim',
-        tag='0.1.2',
-        requires = {
-            {'nvim-lua/plenary.nvim'}
+require("lazy").setup({
+    "wbthomason/packer.nvim",
+    "lewis6991/gitsigns.nvim",
+    "mbbill/undotree",
+    "tpope/vim-fugitive",
+    "hrsh7th/cmp-nvim-lua",
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    { 
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("catppuccin")
+        end
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets"
         }
-    })
-    use({
-        'nvim-treesitter/nvim-treesitter',
-        {run = ':TSUpdate'}
-    })
-    use({
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            {'neovim/nvim-lspconfig'},
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'L3MON4D3/LuaSnip'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'rafamadriz/friendly-snippets'}
-        }
-    })
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+    },
+})
 
 -- git signs
 
-require('gitsigns').setup {
+require("gitsigns").setup {
     signs = {
-        add          = { text = '│' },
-        change       = { text = '│' },
-        delete       = { text = '-' },
-        topdelete    = { text = '-' },
-        changedelete = { text = '~' },
-        untracked    = { text = '|' },
+        add          = { text = "│" },
+        change       = { text = "│" },
+        delete       = { text = "-" },
+        topdelete    = { text = "-" },
+        changedelete = { text = "~" },
+        untracked    = { text = "|" },
     },
     signcolumn = true,
     numhl      = false,
@@ -195,19 +199,19 @@ require('gitsigns').setup {
     current_line_blame = false,
     current_line_blame_opts = {
         virt_text = true,
-        virt_text_pos = 'eol',
+        virt_text_pos = "eol",
         delay = 1,
         ignore_whitespace = false,
     },
-    current_line_blame_formatter = '    ~ <author>, <author_time:%Y-%m-%d %I:%M%p> - <summary>',
+    current_line_blame_formatter = "    ~ <author>, <author_time:%Y-%m-%d %I:%M%p> - <summary>",
     sign_priority = 6,
     update_debounce = 100,
     status_formatter = nil,
     max_file_length = 4000000000,
     preview_config = {
-        border = 'single',
-        style = 'minimal',
-        relative = 'cursor',
+        border = "single",
+        style = "minimal",
+        relative = "cursor",
         row = 0,
         col = 1
     },
@@ -218,10 +222,10 @@ require('gitsigns').setup {
 
 -- lsp
 
-local lsp = require('lsp-zero').preset({})
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
-local lsp_config = require('lspconfig')
+local lsp = require("lsp-zero").preset({})
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
+local lsp_config = require("lspconfig")
 
 vim.diagnostic.config({
     virtual_text = true,
@@ -234,28 +238,28 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
         end,
     },
     sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp' },
-        { name = 'nvim_lua' },
-        { name = 'buffer', max_item_count = 3 },
-        { name = 'luasnip' },
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "buffer", max_item_count = 3 },
+        { name = "luasnip" },
     },
     mapping = {
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-j>'] = cmp.mapping.scroll_docs(4),
-        ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<Tab>'] = nil,
-        ['<C-Space>'] = nil,
-        ['<S-Tab>'] = nil,
-        ['<Enter>'] = nil,
+        ["<C-f>"] = cmp_action.luasnip_jump_forward(),
+        ["<C-b>"] = cmp_action.luasnip_jump_backward(),
+        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+        ["<C-j>"] = cmp.mapping.scroll_docs(4),
+        ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+        ["<Tab>"] = nil,
+        ["<C-Space>"] = nil,
+        ["<S-Tab>"] = nil,
+        ["<Enter>"] = nil,
     },
 })
 
@@ -266,10 +270,10 @@ require("luasnip.loaders.from_vscode").lazy_load()
 -- lsp
 
 lsp.set_sign_icons({
-    error = ' ',
-    warn = ' ',
-    info = ' ',
-    hint = ' ⚑'
+    error = " ",
+    warn = " ",
+    info = " ",
+    hint = " ⚑"
 })
 
 lsp.on_attach(function(_, bufnr)
@@ -290,7 +294,7 @@ end)
 -- lsp servers
 
 lsp_config.pylsp.setup{
-    root_dir = lsp_config.util.root_pattern('.git'),
+    root_dir = lsp_config.util.root_pattern(".git"),
     settings = {
         single_file_support = false,
         pylsp = {
@@ -330,18 +334,18 @@ lsp.setup()
 
 -- telescope
 
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fa', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', telescope_builtin.git_files, {})
-vim.keymap.set('n', '<leader>fb', telescope_builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', ":Telescope help_tags<CR>", {})
-vim.keymap.set('n', '<leader>fp', function()
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>fa", telescope_builtin.find_files, {})
+vim.keymap.set("n", "<leader>ff", telescope_builtin.git_files, {})
+vim.keymap.set("n", "<leader>fb", telescope_builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", {})
+vim.keymap.set("n", "<leader>fp", function()
     telescope_builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
 -- treesitter
 
-require'nvim-treesitter.configs'.setup {
+require"nvim-treesitter.configs".setup {
     sync_install = false,
     auto_install = true,
     highlight = {
@@ -362,14 +366,14 @@ require'nvim-treesitter.configs'.setup {
 
 -- undotree
 
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- nvim tree
 
-require'nvim-tree'.setup()
+require"nvim-tree".setup()
 
 -- lualine
 
-require('lualine').setup {
-    extensions = { 'nvim-tree', 'fugitive' }
+require("lualine").setup {
+    extensions = { "nvim-tree", "fugitive" }
 }
