39517e8e3e943d2fb37147bba8ded76965091f90
Aditya Azad <adityaazad121@gmail.com>
2025-01-01 11:13:18 -0500

    add dadbod

--- nvim/init.lua
+++ nvim/init.lua
@@ -165,6 +165,22 @@ require("lazy").setup({
     "nvim-telescope/telescope-ui-select.nvim",
     "tpope/vim-sleuth",
     {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = {
+            { 'tpope/vim-dadbod',                     lazy = true },
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
+        },
+        cmd = {
+            'DBUI',
+            'DBUIToggle',
+            'DBUIAddConnection',
+            'DBUIFindBuffer',
+        },
+        init = function()
+            vim.g.db_ui_use_nerd_fonts = 1
+        end,
+    },
+    {
         "mfussenegger/nvim-dap",
         dependencies = {
             "leoluz/nvim-dap-go",
@@ -388,6 +404,13 @@ cmp.setup({
         ["<S-Tab>"] = nil,
         ["<Enter>"] = nil,
     },
+})
+
+cmp.setup.filetype({ "sql" }, {
+    sources = {
+        { name = "vim-dadbod-completion" },
+        { name = "buffer" }
+    }
 })
 
 -- luasnip
