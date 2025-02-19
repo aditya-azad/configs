4f8194371e5a2d23735035541f92e4fa1609bc38
Aditya Azad <adityaazad121@gmail.com>
2025-02-19 12:04:20 +0530

    play battery low sound

--- dunst/battery_low_notify.sh
+++ dunst/battery_low_notify.sh
@@ -7,6 +7,7 @@ while true; do
     battery_level=$(acpi -b | grep -P -o '[0-9]+(?=%)' | head -n1)
     if [ $battery_level -le $THRESHOLD ] && [ $last_notification -eq 0 ]; then
         notify-send -a Battery -u critical "Battery Low"
+        aplay ~/code/configs/dunst/battery-low.wav
         last_notification=1
     elif [ $battery_level -gt $THRESHOLD ] && [ $last_notification -eq 1 ]; then
         last_notification=0
