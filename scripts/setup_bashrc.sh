2adb745be8445cd17d2f3805e5b0597da7dfe08d
Aditya Azad <adityaazad121@gmail.com>
2025-06-23 11:17:20 -0400

    add bash setup script, add font

---
+++ scripts/setup_bashrc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASHRC_FILE="$HOME/.bashrc"
+
+# shortcuts
+echo "alias sdn='shutdown now'" >> "$BASHRC_FILE"
+echo "alias start='xdg-open'" >> "$BASHRC_FILE"
+
+# editor
+echo "alias vim='nvim'" >> "$BASHRC_FILE"
+echo "alias vi='nvim'" >> "$BASHRC_FILE"
+echo "export EDITOR=nvim" >> "$BASHRC_FILE"
+
+# vim bindings
+echo "set -o vi" >> "$BASHRC_FILE"
