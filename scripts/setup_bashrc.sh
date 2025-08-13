f97094f2f007ef9a444bf57c2996d9f8123afd55
Aditya Azad <adityaazad121@gmail.com>
2025-08-13 08:18:22 -0400

    add robotics shortcuts

--- scripts/setup_bashrc.sh
+++ scripts/setup_bashrc.sh
@@ -19,3 +19,7 @@ echo "bind -m vi-insert 'Control-l: clear-screen'" >> "$BASHRC_FILE"
 # documents
 echo "alias todo='nvim ~/database/workspace/inbox.md'" >> "$BASHRC_FILE"
 echo "alias ideas='nvim ~/database/workspace/ideas.md'" >> "$BASHRC_FILE"
+
+# robotics
+echo 'export PATH=${PATH}:/usr/src/tensorrt/bin/' >> "$BASHRC_FILE"
+echo "alias r2='source /opt/ros/humble/setup.bash'" >> "$BASHRC_FILE"
