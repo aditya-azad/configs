7558828d1667d1483b14185ab528f7c4aea6518d
Aditya Azad <adityaazad121@gmail.com>
2025-10-05 09:19:18 -0400

    fix number of workspaces

--- scripts/8_setup_gnome.sh
+++ scripts/8_setup_gnome.sh
@@ -36,6 +36,8 @@ gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
 
 # workspace
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
+gsettings set org.gnome.mutter dynamic-workspaces false
+
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-1 "['<Super><Shift>1']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-2 "['<Super><Shift>2']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-3 "['<Super><Shift>3']"
@@ -45,7 +47,7 @@ gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-6 "['<Super><Sh
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-7 "['<Super><Shift>7']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-8 "['<Super><Shift>8']"
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-9 "['<Super><Shift>9']"
-gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>10']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-10 "['<Super><Shift>0']"
 
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
@@ -56,7 +58,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>10']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-10 "['<Super>0']"
 
 # dock
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
