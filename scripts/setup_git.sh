733a838bc5dca2bfc722a014a3bea5389062e38e
Aditya Azad <adityaazad121@gmail.com>
2025-10-24 07:42:22 -0400

    add option to use personal ssh key for git setup

--- scripts/setup_git.sh
+++ scripts/setup_git.sh
@@ -5,10 +5,13 @@ set -e
 git config --global user.name "$USER_FULL_NAME"
 git config --global user.email "$USER_EMAIL_ADDRESS"
 
-ssh-keygen -t ed25519 -C "$git_email" -f ~/.ssh/id_ed25519
+read -p "Do you want to use your own existing SSH key pair? [y/n]: "  ans
+if [[ "$ans" == "n" || "$ans" == "N" ]]; then
+  ssh-keygen -t ed25519 -C "$USER_EMAIL_ADDRESS" -f ~/.ssh/id_ed25519
 
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_ed25519
 
-echo "Here is your SSH public key (add this to your GitHub account):"
-cat ~/.ssh/id_ed25519.pub
+  echo "Here is your SSH public key (add this to your GitHub account):"
+  cat ~/.ssh/id_ed25519.pub
+fi
