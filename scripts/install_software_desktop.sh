d54bc5c55d1cf2d1cd767a124c33a1edf68a093e
Aditya Azad <adityaazad121@gmail.com>
2025-10-27 11:24:48 -0400

    fix cuda installation

--- scripts/install_software_desktop.sh
+++ scripts/install_software_desktop.sh
@@ -39,6 +39,9 @@ sudo apt install -y calibre
 # brave
 curl -fsS https://dl.brave.com/install.sh | sh
 
+# nvidia driver
+sudo apt install nvidia-driver-570
+
 # cuda 12.6
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
@@ -47,7 +50,6 @@ sudo dpkg -i cuda-repo-ubuntu2204-12-6-local_12.6.0-560.28.03-1_amd64.deb
 sudo cp /var/cuda-repo-ubuntu2204-12-6-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update -y
 sudo apt-get -y install cuda-toolkit-12-6
-sudo apt-get install -y nvidia-open
 echo "export PATH=${PATH}:/usr/local/cuda-12.6/bin" >> "$BASHRC_FILE"
 echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-12.6/lib64" >> "$BASHRC_FILE"
 
