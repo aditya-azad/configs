ec7ea29baca211bc3f4ee95adb4befb9c0ab3d4d
Aditya Azad <adityaazad121@gmail.com>
2025-12-19 10:09:43 -0500

    backtrack

--- scripts/setup_symlinks_common.sh
+++ scripts/setup_symlinks_common.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-mkdir -p "$HOME/.config/nvim"
 ln -s "$HOME/code/configs/nvim" "$HOME/.config/nvim"
 rm -rf $HOME/.config/zellij
-mkdir -p "$HOME/.config/zellij"
 ln -s "$HOME/code/configs/zellij" "$HOME/.config/zellij"
-mkdir -p "$HOME/.config/btop"
 ln -s "$HOME/code/configs/btop" "$HOME/.config/btop"
