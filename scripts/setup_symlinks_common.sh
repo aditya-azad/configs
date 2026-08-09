a0372f916333d06c441e3f52e4f033e088104860
Aditya Azad <adityaazad121@gmail.com>
2026-08-09 17:28:28 -0400

    add skills

--- scripts/setup_symlinks_common.sh
+++ scripts/setup_symlinks_common.sh
@@ -6,3 +6,5 @@ ln -s "$HOME/code/configs/nvim" "$HOME/.config/nvim"
 rm -rf $HOME/.config/zellij
 ln -s "$HOME/code/configs/zellij" "$HOME/.config/zellij"
 ln -s "$HOME/code/configs/btop" "$HOME/.config/btop"
+mkdir -p "$HOME/.omp/agent"
+ln -s "$HOME/code/configs/pi/skills" "$HOME/.omp/agent/skills"
