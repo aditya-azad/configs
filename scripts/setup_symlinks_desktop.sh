79a2b470685fd488ca4c8d82ea0618a949d1444f
Aditya Azad <adityaazad121@gmail.com>
2026-08-16 21:42:09 -0400

    setup syncthing symlinks

--- scripts/setup_symlinks_desktop.sh
+++ scripts/setup_symlinks_desktop.sh
@@ -5,3 +5,8 @@ set -e
 rm -rf "$HOME/.config/kitty"
 ln -s "$HOME/code/configs/kitty" "$HOME/.config/kitty"
 ln -s "$HOME/code/configs/refree" "$HOME/.refree"
+mkdir -p "$HOME/.local/state/syncthing"
+for f in cert.pem config.xml https-cert.pem https-key.pem key.pem; do
+  rm -f "$HOME/.local/state/syncthing/$f"
+  ln -s "$HOME/database/docs/passwords-keys/syncthing-legion-7i/$f" "$HOME/.local/state/syncthing/$f"
+done
