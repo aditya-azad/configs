d74ef795ffcda7a02b9ef63c3794ac0c573a1838
Aditya Azad <adityaazad121@gmail.com>
2026-08-17 08:15:44 -0400

    remove others

--- scripts/install-android.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-./scripts/install_compilers_android.sh
-./scripts/install_software_android.sh
-./scripts/setup_symlinks_common.sh
-./scripts/setup_bashrc_common.sh
-./scripts/setup_git.sh
