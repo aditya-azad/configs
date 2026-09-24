d3ecf59c349391dda579889c11d98272cc7c71d1
Aditya Azad <adityaazad121@gmail.com>
2026-09-24 16:01:27 -0400

    cleanup

--- bash/tasks/greetd.sh
+++ bash/tasks/greetd.sh
@@ -18,3 +18,7 @@ if [[ "$cfg_changed" == 1 ]]; then
 fi
 
 sudo systemctl enable greetd.service 2>/dev/null || true
+
+if systemctl list-unit-files 2>/dev/null | grep -qE '^gdm\.service[[:space:]]'; then
+  sudo systemctl disable gdm.service 2>/dev/null || true
+fi
