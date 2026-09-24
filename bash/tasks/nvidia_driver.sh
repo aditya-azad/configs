8bc69f831ce24266963f078299eeddb2fb08728b
Aditya Azad <adityaazad121@gmail.com>
2026-09-24 14:18:33 -0400

    cleanup

--- bash/tasks/nvidia_driver.sh
+++ bash/tasks/nvidia_driver.sh
@@ -2,6 +2,4 @@
 set -euo pipefail
 . "$(dirname "${BASH_SOURCE[0]}")/../lib/common.sh"
 
-sudo add-apt-repository -y ppa:graphics-drivers/ppa
-sudo apt-get update
-sudo apt-get install -y "nvidia-driver-$NVIDIA_DRIVER_VERSION"
+sudo ubuntu-drivers install
